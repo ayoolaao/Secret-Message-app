a509846cf256e793d97ded3ae8061f171b2de862
Ayoolaao <ao.abimbola@gmail.com>
2016-11-24 15:46:50 -0600

    switched to pycharm ide

--- rename_files.py
+++ rename_files.py
@@ -3,6 +3,9 @@ def rename_files():
     #1. get file names from dir
     file_list = os.listdir("/Users/ayoolaao/Desktop/udacity/Full Stack Web Developer Nanodegree/Intro to python/Secret Message app/prank")
     print(file_list)
+    
     #2. for each file, rename filename
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
 
 rename_files()
