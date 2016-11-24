2534623ce3ded3a31ae121c1c22b13910acb8c64
Ayoolaao <ao.abimbola@gmail.com>
2016-11-24 15:06:52 -0600

    added code to get file name for a dir

---
+++ rename_files.py
@@ -0,0 +1,8 @@
+import os
+def rename_files():
+    #1. get file names from dir
+    file_list = os.listdir("/Users/ayoolaao/Desktop/udacity/Full Stack Web Developer Nanodegree/Intro to python/Secret Message app/prank")
+    print(file_list)
+    #2. for each file, rename filename
+
+rename_files()
