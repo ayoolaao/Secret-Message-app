08af2f5a8d1fa0697e6d507986c33c4015fe440a
Ayoolaao <ao.abimbola@gmail.com>
2016-11-24 18:26:52 -0600

    completed code to rename files

--- rename_files.py
+++ rename_files.py
@@ -1,11 +1,21 @@
 import os
 def rename_files():
+
     #1. get file names from dir
+
     file_list = os.listdir("/Users/ayoolaao/Desktop/udacity/Full Stack Web Developer Nanodegree/Intro to python/Secret Message app/prank")
     print(file_list)
+
+    saved_path = os.getcwd()
+    print("current dir:" + saved_path)
+    os.chdir("/Users/ayoolaao/Desktop/udacity/Full Stack Web Developer Nanodegree/Intro to python/Secret Message app/prank")
     
     #2. for each file, rename filename
+
     for file_name in file_list:
+        print ("Old name - " + file_name)
+        print ("New name - " + file_name.translate(None, "0123456789"))
         os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
 
 rename_files()
